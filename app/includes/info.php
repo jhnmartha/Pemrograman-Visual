29d56b8e0bdd9591406144863f57e30d7263085b
jhnmartha <113563777+jhnmartha@users.noreply.github.com>
2025-05-02 09:09:11 +0700

    Delete app directory

--- app/includes/info.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SOFTWARE_NAME', 'Weather Forecast');
-define('SOFTWARE_AUTHOR', 'Usama Sarwar');
-define('SOFTWARE_URL', 'https://usamasarwar.github.io');
-define('SOFTWARE_VERSION', '1.0.0');
